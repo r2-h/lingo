347c9503382708dddb3b8a20481e6587c7134e14
Artur Khareksian <r2mail1122@gmail.com>
2024-04-26 14:19:25 +0300

    feat: add shop page

--- app/(main)/shop/page.tsx
+++ app/(main)/shop/page.tsx
@@ -1,4 +1,43 @@
-const ShopPage = () => {
-  return <div>Shop</div>
+import { FeedWrapper } from "@/components/FeedWrapper"
+import { StickyWrapper } from "@/components/StickyWrapper"
+import { UserProgress } from "@/components/UserProgress"
+import { getUserProgress } from "@/db/queries"
+import Image from "next/image"
+import { redirect } from "next/navigation"
+import { Items } from "./_components/page"
+
+const ShopPage = async () => {
+  const [userProgress] = await Promise.all([getUserProgress()])
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses")
+  }
+
+  return (
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={userProgress.activeCourse}
+          hasActiveSubscription={false}
+          hearts={userProgress.hearts}
+          points={userProgress.points}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <div className="w-full flex flex-col items-center">
+          <Image src="/shop.svg" height={90} width={90} alt="Shop" />
+          <h1 className="text-center font-bold text-neutral-800 text-2xl my-6">Shop</h1>
+          <p className="text-muted-foreground text-center text-lg mb-6">
+            Spend your points on cool stuff
+          </p>
+          <Items
+            hearts={userProgress.hearts}
+            points={userProgress.points}
+            hasActiveSubscription={false}
+          />
+        </div>
+      </FeedWrapper>
+    </div>
+  )
 }
 export default ShopPage
